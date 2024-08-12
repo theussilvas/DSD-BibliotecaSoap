dce3d01ff78a8548f9db53d6203d1acce08a6d73
Giovanna Lino <106554500+gigigabytes@users.noreply.github.com>
2024-08-12 15:01:54 -0300

    Update server.py

--- server.py
+++ server.py
@@ -70,6 +70,6 @@ wsgi_app = WsgiApplication(application)
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    server = make_server('192.168.0.9', 8000, wsgi_app)
+    server = make_server('10.25.1.213', 8000, wsgi_app)
     print("Rodando na porta 8000")
     server.serve_forever()
